7b69f8d07399e09799e53094ddea2d52ef8decf4
alongigi <giga@post.bgu.ac.il>
2018-06-30 23:00:32 +0300

    pre processing

--- PreProcessing.py
+++ PreProcessing.py
@@ -2,10 +2,11 @@
     Filling the missing values (NA) with mean.
     df - Data Frame
 """
+
+
 def fillMissingValuesWithMean(df):
     df_without_missing_values = df.fillna(df.mean())
     return df_without_missing_values
-
 
 
 """
@@ -13,7 +14,9 @@ Returns the mean of specific feature
 df - Data Frame
 feature_name - The mean of the feature
 """
-def getFeatureMean(df, feature_name):
+
+
+def get_feature_mean(df, feature_name):
     return df[feature_name].mean()
 
 
@@ -22,8 +25,11 @@ Returns the STD of specific feature
 df - Data Frame
 feature_name - The STD of the feature
 """
-def getFeatureSTD(df, feature_name):
+
+
+def get_feature_std(df, feature_name):
     return df[feature_name].std()
+
 
 def normalize(df):
     "Makes a copy of the data frame."
@@ -31,9 +37,8 @@ def normalize(df):
     "For each feature in we normalize the data, except for those feature who are not numeric."
     for feature_name in df.columns:
         try:
-            mean = getFeatureMean(df, feature_name)
-            std = getFeatureSTD(df, feature_name)
-
+            mean = get_feature_mean(df, feature_name)
+            std = get_feature_std(df, feature_name)
             "Inserting to the feature the normalized value."
             result[feature_name] = (df[feature_name] - mean) / std
         except:
@@ -41,14 +46,12 @@ def normalize(df):
             continue
     return result
 
-
-
+"""
+Making "kibutz data" by using builded function - groupby which included in the data frame variable and replacing it
+with its mean
+"""
 
 
 def kibuz_data(df):
     df = df.groupby("country").mean()
-    df.reset_index(inplace=True)
     return df
-
-def getNumberOfRows(df, feature):
-    return df[feature].count()
