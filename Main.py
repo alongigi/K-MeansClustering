f273906a2805d299812e4202ce9e9b26d15915f2
alongigi <giga@post.bgu.ac.il>
2018-07-01 23:08:12 +0300

    main

--- Main.py
+++ Main.py
@@ -1,12 +1,22 @@
 from Controller import Controller
 from View import View
 
+"""
+The main function, from here all the program stars.
+Initializing the controller object
+Initializing the view object
+Inserting to the view the controller and then starting the view.
+"""
 
-def Main():
+
+def main():
     c = Controller()
     v = View(c)
     v.start()
 
 
+"""
+If running this file the main function will be executed.
+"""
 if __name__ == '__main__':
-    Main()
+    main()
